0dcbf3d7c98984b1473e55b1d0442d091808c75f
wedmakkk <63804210+wedmakkk@users.noreply.github.com>
2024-10-04 21:44:05 +0300

    Add files via upload

---
+++ task3ready.cpp
@@ -0,0 +1,163 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <locale.h>
+
+struct node
+{
+	char inf[256];  // полезная информация
+	struct node* next; // ссылка на следующий элемент 
+};
+
+struct node* head = NULL, * last = NULL, * f = NULL, * prev = NULL, *predlast = NULL; // указатели на первый и последний элементы списка
+int dlinna = 0;
+
+void spstore(void), review(void), del(char* name);
+
+char find_el[256];
+struct node* find(char* name); // функция нахождения элемента
+struct node* get_struct(void); // функция создания элемента
+
+
+
+struct node* get_struct(void)
+{
+	struct node* p = NULL;
+	char s[256];
+	if ((p = (node*)malloc(sizeof(struct node))) == NULL)  // выделяем память под новый элемент списка
+	{
+		printf("Ошибка при распределении памяти\n");
+		exit(1);
+	}
+	printf("Введите название объекта: \n");   // вводим данные
+	scanf("%s", s);
+	if (*s == 0)
+	{
+		printf("Запись не была произведена\n");
+		return NULL;
+	}
+	strcpy(p->inf, s);
+
+	p->next = NULL;
+
+	return p;		// возвращаем указатель на созданный элемент
+}
+
+/* Последовательное добавление в список элемента (в конец)*/
+void spstore(void)
+{
+	struct node* p = NULL;
+	p = get_struct();
+	if (head == NULL && p != NULL)	// если списка нет, то устанавливаем голову списка
+	{
+		head = p;
+		last = p;
+	}
+	else if (head != NULL && p != NULL) // список уже есть, то вставляем в конец
+	{
+		last->next = p;
+		predlast = last;
+		last = p;
+	}
+	return;
+}
+
+
+
+/* Просмотр содержимого списка. */
+void review(void)
+{
+	struct node* struc = head;
+	if (head == NULL)
+	{
+		printf("Список пуст\n");
+	}
+	while (struc)
+	{
+		printf("Имя - %s, \n", struc->inf);
+		struc = struc->next;
+	}
+	return;
+}
+
+
+
+
+/* Поиск элемента по содержимому. */
+struct node* find(char* name)
+{
+	struct node* struc = head;
+	if (head == NULL)
+	{
+		printf("Список пуст\n");
+	}
+	while (struc)
+	{
+		if (strcmp(name, struc->inf) == 0)
+		{
+			return struc;
+		}
+		struc = struc->next;
+	}
+	printf("Элемент не найден\n");
+	return NULL;
+}
+
+/* Удаление элемента по содержимому. */
+void del(void)
+{
+	struct node* struc = head; // указатель, проходящий по списку установлен на начало списка
+	struct node* prev = NULL; // указатель на предшествующий удаляемому элемент
+
+	if (head == NULL) // если голова списка равна NULL, то список пуст
+	{
+		printf("Список пуст\n");
+		return;
+	}
+
+	if (head->next == NULL) // если в списке только один элемент
+	{
+		free(head);
+		head = NULL;
+		return;
+	}
+
+	while (struc->next != NULL) // проходим по списку до последнего элемента
+	{
+		prev = struc;
+		struc = struc->next;
+	}
+
+	prev->next = NULL; // отсекаем последний элемент
+	last = prev;
+	free(struc); // освобождаем память для последнего элемента
+}
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	spstore();
+	spstore();
+	spstore();
+	review();
+	while (1)
+	{
+		int nu;
+		printf("\nВыберите вариант дальнейшего действия:\n1. убрать 1 элемент\n2. добавить элемент\n3. вывод\n");
+		scanf_s("%i", &nu);
+		if (nu == 1)
+		{
+			del();
+		}
+		if (nu == 2)
+		{
+			spstore();
+		}
+		if (nu == 3)
+		{
+			review();
+		}
+	}
+	return 0;
+}
